6cfae6c96db8067e05b0f1443bc4f0fa6fc82a45
Koustav Das <110321920+devkoustavdas@users.noreply.github.com>
2023-10-07 22:55:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,27 +20,37 @@ var d = document.getElementById("d");
 var correct = "o";
 var comment = document.getElementById("comment");;
 var i = -1;
+var questionArray = [];
 
 function next() {
     i++;
-    a.style.borderColor = "transparent";
-    b.style.borderColor = "transparent";
-    c.style.borderColor = "transparent";
-    d.style.borderColor = "transparent";
-    comment.style.transform = "scale(0)";
-
-    questionNo.innerHTML = `${i+1}`;
     fetch(fullURL)
     .then((res) => res.text())
     .then((rep) => {
         let data = JSON.parse(rep.substr(47).slice(0, -2));
-        question.textContent = data.table.rows[i].c[1].v;
-        a.textContent = data.table.rows[i].c[2].v;
-        b.textContent = data.table.rows[i].c[3].v;
-        c.textContent = data.table.rows[i].c[4].v;
-        d.textContent = data.table.rows[i].c[5].v;
-        correct = data.table.rows[i].c[6].v;
-        comment.innerHTML = data.table.rows[i].c[8].v;
+        if (data.table.rows[i].c[1]) {
+          questionNo.innerHTML = `${i + 1}`;
+          a.style.borderColor = "transparent";
+          b.style.borderColor = "transparent";
+          c.style.borderColor = "transparent";
+          d.style.borderColor = "transparent";
+          comment.style.transform = "scale(0)";
+          question.textContent = data.table.rows[i].c[1].v;
+          a.textContent = data.table.rows[i].c[2].v;
+          b.textContent = data.table.rows[i].c[3].v;
+          c.textContent = data.table.rows[i].c[4].v;
+          d.textContent = data.table.rows[i].c[5].v;
+          correct = data.table.rows[i].c[6].v;
+          if (correct == "a") questionArray.push([data.table.rows[i].c[1].v, data.table.rows[i].c[2].v]);
+          else if (correct == "b") questionArray.push([data.table.rows[i].c[1].v, data.table.rows[i].c[3].v]);
+          else if (correct == "c") questionArray.push([data.table.rows[i].c[1].v, data.table.rows[i].c[4].v]);
+          else if (correct == "d") questionArray.push([data.table.rows[i].c[1].v, data.table.rows[i].c[5].v]);
+          
+          comment.innerHTML = data.table.rows[i].c[8].v;
+        }
+        else {
+          showAnswer();
+        }
     });
 }
 
@@ -65,10 +75,21 @@ window.onload = function () {
       });
 };
 
-function screenSize() {
-  var screenWidth = window.innerWidth;
-  if (screenWidth > 1200) {
-    
+var answerDisplay = document.getElementById("answer-display");
+function showAnswer() {
+  answerDisplay.style.display = "block";
+  document.getElementById("questionPanel").style.display = "none";
+  for(var i = 0; i<questionArray.length; i++) {
+      var quizdiv = document.createElement("div");
+      quizdiv.className = "quiz";
+      var questiondiv = document.createElement("div");
+      questiondiv.className = "questions";
+      questiondiv.textContent = questionArray[i][0];
+      var optiondiv = document.createElement("div");
+      optiondiv.className = "correctoptiondiv";
+      optiondiv.textContent = questionArray[i][1];
+      quizdiv.appendChild(questiondiv);
+      quizdiv.appendChild(optiondiv);
+      answerDisplay.appendChild(quizdiv);
   }
 }
-screenSize();
